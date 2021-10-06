af0fe267ebec2e9977b4d422f53985f7fc91df3c
Daturaqq <anikaevilya@gmail.com>
2021-10-06 13:04:26 +0300

    2.1.2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,15 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld firstBean =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld secondBean =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        boolean helloWorld = firstBean == secondBean;
+        System.out.println("Сравнение бинов HelloWorld по ссылке = " + helloWorld);
+        Cat firstCat = (Cat) applicationContext.getBean("cat");
+        Cat secondCat = (Cat) applicationContext.getBean("cat");
+        boolean cat = firstCat == secondCat;
+        System.out.println("Сравнение бинов Cat по ссылке = " + cat);
     }
 }
